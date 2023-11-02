0180995fbcbfbe34c3651c133f90549e84c26915
Oziel Guerra <ozi_guerra@hotmail.com>
2023-11-02 13:37:46 -0700

    File re-organization, routing implementation, style implamentation

--- index.js
+++ index.js
@@ -8,8 +8,24 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
-});
+    let blogPosts = [
+        {
+            title: 'Who am I?',
+            body: '...',
+            author: 'Oziel Guerra',
+            publishedAt: new Date('2016-03-19'),
+            createdAt: new Date('2016-03-19')
+        },];
+        res.render("index.ejs", { posts: blogPosts });
+  });
+
+  app.get("/about", (req, res) => {
+    res.render("about.ejs");
+  });
+
+  app.get("/contact", (req, res) => {
+    res.render("contact.ejs");
+  });
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
