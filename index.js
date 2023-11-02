bd52fca0c0e81ad9d496a59df8f58cbba62a4d80
Oziel Guerra <ozi_guerra@hotmail.com>
2023-11-02 13:04:07 -0700

    Project setup cont.

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.render("index.ejs");
 });
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
